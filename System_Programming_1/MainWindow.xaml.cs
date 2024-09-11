7a5d6b380f00d51f42d978b3f703bcbfdc7c02dc
lenovo <lenovo@DESKTOP-O95QVK8>
2024-09-11 10:34:50 +0400

    Add project files.

---
+++ System_Programming_1/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace System_Programming_1;
+
+public partial class MainWindow : NavigationWindow
+{
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        var page = new Page1();
+        NavigationService.Navigate(page);
+    }
+
+}
